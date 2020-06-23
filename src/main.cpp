718786c588f00e7c8feddfc68115e12639893744
Dan Abad <dan.r.abad@gmail.com>
2020-06-22 20:38:14 -0500

    Add command line args to main

--- src/main.cpp
+++ src/main.cpp
@@ -336,7 +336,7 @@ struct TimeKeeper {
     }
 };
 
-int main() {
+int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
